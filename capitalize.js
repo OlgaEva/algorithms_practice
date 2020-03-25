9e839de566b22aa6bdb1b8680356073f55ced3cd
OlgaEva <olga.rosas@gmail.com>
2020-03-25 15:38:05 -0400

    add capitalize directions

---
+++ capitalize.js
@@ -0,0 +1,10 @@
+/* Directions
+
+Write a function that accepts a string. The function should capitalize the first letter of each word
+then return the capitalized string.
+
+*/
+
+function capitalize(str) {
+    
+}
