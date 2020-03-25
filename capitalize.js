1a2288ef56d9e6e88a5866742ad23b31b16d49da
OlgaEva <olga.rosas@gmail.com>
2020-03-25 15:43:45 -0400

    add capitalize solution

--- capitalize.js
+++ capitalize.js
@@ -6,5 +6,12 @@ then return the capitalized string.
 */
 
 function capitalize(str) {
-    
-}
+    var splitzville = str.split(' ');
+    var newString = [];
+  
+    for(elem of splitzville){
+      newString.push(elem[0].toUpperCase() + elem.slice(1))
+    }
+  
+    return newString.join(' ');
+  }
